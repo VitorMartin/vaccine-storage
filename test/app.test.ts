dea7aed89277e33b2fe8add6102bb32c67c79d46
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-14 21:08:51 -0300

    Adding Supertest to test suite

---
+++ test/app.test.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+
+import config from 'config'
+const appConfig : any = config.get('appConfig')
+const basePath : string = `/${appConfig.name}/`
+
+import { app } from '../src/app'
+import request, { Response } from 'supertest'
+
+describe(`Server is working: ${appConfig.name}`, () => {
+    test('ping ==> server is listenning', async () => {        
+        const res : Response = await request(app).get(path.join(basePath, '/ping'))
+        
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual(appConfig)
+    })
+});

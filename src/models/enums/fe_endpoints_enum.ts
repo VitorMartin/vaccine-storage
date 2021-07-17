88b82385ee8b4055dca56fae4729f9012fa3f33f
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-17 19:45:19 -0300

    Adjusting endpoints enum

--- src/models/enums/fe_endpoints_enum.ts
+++ src/models/enums/fe_endpoints_enum.ts
@@ -5,6 +5,7 @@ const basePath: string = routerEndpoint.API_BASE_PATH
 export class feEndpoint { // FRONTEND ENDPOINTS. Class, but acts like enum: cannot parse variables as enums
     static SERVER_ROOT: string = routerEndpoint.ROOT
     static API_BASE_PATH: string = routerEndpoint.API_BASE_PATH
-    static PING : string = `${routerEndpoint.API_BASE_PATH}/ping`
-    static ITEM : string = `${routerEndpoint.API_BASE_PATH}/item`
+    static PING : string = `${routerEndpoint.API_BASE_PATH}/${routerEndpoint.PING}`
+    static ITEM : string = `${routerEndpoint.API_BASE_PATH}/${routerEndpoint.ITEM}`
+    static ALL_ITEMS : string = `${routerEndpoint.API_BASE_PATH}/${routerEndpoint.ALL_ITEMS}`
 }

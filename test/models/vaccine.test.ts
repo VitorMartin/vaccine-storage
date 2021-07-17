b84cce8deb93feadc0750faa1c3b8716bc495e6e
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-16 21:29:14 -0300

    Improving enum import

--- test/models/vaccine.test.ts
+++ test/models/vaccine.test.ts
@@ -1,15 +1,15 @@
 import { VaccineModel } from '../../src/models/vaccine'
-import { brand, mfr, name } from '../../src/models/enums/vaccine'
+import * as vaccineEnums from '../../src/models/enums/vaccine'
 
 describe('Models ==> vaccine', () => {
     test('instance vaccine', () => {
         const fabDate = new Date(2021, 6, 1)
         const dueDate = new Date(fabDate.getFullYear(), fabDate.getMonth() + 12, fabDate.getDate())
-        const vaccine = new VaccineModel(brand.SINOVAC, name.BUTANVAC, mfr.BUTANTAN, 12, fabDate, dueDate)
+        const vaccine = new VaccineModel(vaccineEnums.brand.SINOVAC, vaccineEnums.name.BUTANVAC, vaccineEnums.mfr.BUTANTAN, 12, fabDate, dueDate)
         
-        expect(vaccine.brand).toBe(brand.SINOVAC)
-        expect(vaccine.name).toBe(name.BUTANVAC)
-        expect(vaccine.mfr).toBe(mfr.BUTANTAN)
+        expect(vaccine.brand).toBe(vaccineEnums.brand.SINOVAC)
+        expect(vaccine.name).toBe(vaccineEnums.name.BUTANVAC)
+        expect(vaccine.mfr).toBe(vaccineEnums.mfr.BUTANTAN)
         expect(vaccine.qty).toBe(12)
         expect(vaccine.fabDate).toBe(fabDate)
         expect(vaccine.dueDate).toBe(dueDate)

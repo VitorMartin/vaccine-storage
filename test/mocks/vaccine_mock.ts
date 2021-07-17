6ecf2ab6e426ef2bfe6a50482389e386629b6431
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-16 22:52:39 -0300

    Add mock uuid to vaccine mock

--- test/mocks/vaccine_mock.ts
+++ test/mocks/vaccine_mock.ts
@@ -1,8 +1,10 @@
 import { VaccineModel } from '../../src/models/vaccine_model';
+import { ItemMock } from '../mocks/item_mock';
 import * as vaccineEnums from '../../src/models/enums/vaccine_enum'
 
 export class VaccineMock extends VaccineModel {
     constructor() {
         super(vaccineEnums.brand.SINOVAC, vaccineEnums.name.BUTANVAC, vaccineEnums.mfr.BUTANTAN, 10, new Date(2021, 0, 1), new Date(2022, 12, 31))
+        this.uuid = new ItemMock().uuid
     }
 }

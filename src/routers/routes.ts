d2c85286910886150e4c3f3001c97e7f0b1b3e77
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-17 20:38:37 -0300

    Implementing and testing endpoints for item's CRUD

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -4,6 +4,11 @@ const router: Router = express.Router()
 import config from 'config';
 const appConfig : any = config.get('appConfig')
 import { routerEndpoint as endpoint } from '../models/enums/router_endpoints_enum';
+import { ItemModel } from '../models/item_model';
+import { StorageVolatile } from '../controllers/db/volatile/storage_volatile';
+import { VaccineModel } from '../models/vaccine_model';
+
+const storage = new StorageVolatile()
 
 router.use(function timeLog (req : Request, res : Response, next : NextFunction) {
     console.log({
@@ -20,6 +25,38 @@ router.use(function timeLog (req : Request, res : Response, next : NextFunction)
 
 router.get(endpoint.PING, (req: Request, res: Response) => {
     res.status(200).send(appConfig)
+})
+
+router.post(endpoint.ITEM, (req: Request, res: Response) => {
+    const items: ItemModel[] = []
+    for (let i = 0; i < req.body.items.length; i++) {
+        const itemJSON: JSON = req.body.items[i];
+        const vaccine = VaccineModel.fromJSON(itemJSON)
+        items.push(vaccine)
+    }
+    try {
+        storage.insertItems(items)
+    }
+    catch (pass) {
+        res.status(400).send()
+    }
+    res.status(201).send()
+})
+
+router.get(endpoint.ALL_ITEMS, (req: Request, res: Response) => {
+    res.send(storage.getAllData())
+})
+
+router.get(endpoint.ITEM, (req: Request, res: Response) => {
+    let items: ItemModel[] | object[]
+    try {
+        items = storage.readItem(req.body.attr, req.body.val)
+    }
+    catch (pass) {
+        items = []
+        res.status(400).send(items)
+    }
+    res.send(items)
 })
 
 export = router
--- test/routers/routes.test.ts
+++ test/routers/routes.test.ts
@@ -5,11 +5,53 @@ import { app } from '../../src/app'
 import request, { Response } from 'supertest'
 import { feEndpoint as endpoint } from '../../src/models/enums/fe_endpoints_enum'
 
+import { VaccineMock } from '../mocks/vaccine_mock'
+
 describe(`Router ==> endpoints`, () => {
-    test('api is pinging', async () => {        
-        const res : Response = await request(app).get(endpoint.PING)
-        
-        expect(res.status).toBe(200)
-        expect(res.body).toEqual(appConfig)
+    describe('Ping', () => {
+        test('api is pinging', async () => {
+            const res : Response = await request(app).get(endpoint.PING)
+
+            expect(res.status).toBe(200)
+            expect(res.body).toEqual(appConfig)
+        })
+    })
+
+    describe('Insert items', () => {
+        test('add one item', async () => {
+            const item = new VaccineMock()
+
+            await request(app)
+                .post(endpoint.ITEM)
+                .send({ 'items': [item] })
+            
+            const res: Response = await request(app)
+                .get(endpoint.ITEM)
+                .send({
+                    'attr': 'uuid',
+                    'val': item.uuid
+                })
+            
+            expect(res.body[0].uuid).toEqual(item.uuid)
+        })
+    })
+
+    describe('Read items', () => {
+        test('read one item by uuid', async () => {
+            const item = new VaccineMock()
+
+            await request(app)
+                .post(endpoint.ITEM)
+                .send({ 'items': [item] })
+            
+            const res: Response = await request(app)
+                .get(endpoint.ITEM)
+                .send({
+                    'attr': 'uuid',
+                    'val': item.uuid
+                })
+            
+            expect(res.body[0].uuid).toEqual(item.uuid)
+        })
     })
 });

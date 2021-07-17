b5d5446b70f5cf67ad6a024f33ea447c82e80405
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-17 16:23:02 -0300

    Rename usecase database directory

--- src/controllers/db/volatile/storage_volatile.ts
+++ src/controllers/db/volatile/storage_volatile.ts
@@ -1,5 +1,5 @@
 import { ItemModel } from "../../../models/item_model";
-import { IStorage } from "../../../usecases/repositories/storage_interface";
+import { IStorage } from "../../../usecases/db/storage_interface";
 
 export class StorageVolatile implements IStorage {
     data: ItemModel[]

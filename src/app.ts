202497edd8e1216d07bfe5a570ad229908236c88
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-21 23:32:45 -0300

    Converting router to class

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ export const app : Application = express()
 import config from 'config';
 const appConfig : any = config.get('appConfig')
 
-import router from "./routers/routes";
+import { AppRouter } from "./routers/routes";
 import { routerEndpoint as endpoint } from './models/enums/router_endpoints_enum';
+import { StorageVolatile } from './repositories/volatile/storage_volatile';
+
+const appRouter = new AppRouter(new StorageVolatile())
 
 app.use(express.json())
 
-app.use(endpoint.API_BASE_PATH, router)
+app.use(endpoint.API_BASE_PATH, appRouter.router)
 
 app.get(endpoint.ROOT, (req, res) => {
     res.status(200).send(appConfig)
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,67 +1,71 @@
 import express, { Router, Request, Response, NextFunction } from 'express';
-const router: Router = express.Router()
 
 import config from 'config';
 const appConfig : any = config.get('appConfig')
+
+import { IStorage } from '../interfaces/storage_interface';
 import { routerEndpoint as endpoint } from '../models/enums/router_endpoints_enum';
-import { ItemModel } from '../models/item_model';
-import { StorageVolatile } from '../repositories/volatile/storage_volatile';
 import { VaccineModel } from '../models/vaccine_model';
 import { AddVacs } from '../usecases/addVacs';
 
+export class AppRouter {
+    private storage: IStorage;
+    router: Router;
+    
+    constructor(storage: IStorage) {
+        this.router = express.Router()
+        this.storage = storage
 
-const storage = new StorageVolatile()
+        this.router.use(function timeLog(req: Request, res: Response, next: NextFunction) {
+            console.log({
+                'datetime': new Date().toISOString(),
+                'req': {
+                    'body': req.body,
+                    'method': req.method,
+                    'originalUrl': req.originalUrl
+                }
+            })
 
-router.use(function timeLog (req : Request, res : Response, next : NextFunction) {
-    console.log({
-        'datetime': new Date().toISOString(),
-        'req': {
-            'body': req.body,
-            'method': req.method,
-            'originalUrl': req.originalUrl
-        }
-    })
+            next()
+        })
 
-    next()
-})
+        this.router.get(endpoint.PING, (req: Request, res: Response) => {
+            res.status(200).send(appConfig)
+        })
 
-router.get(endpoint.PING, (req: Request, res: Response) => {
-    res.status(200).send(appConfig)
-})
+        this.router.post(endpoint.VACCINE, (req: Request, res: Response) => {
+            // Validate request ==> validate HTTP Protocol
 
-router.post(endpoint.VACCINE, (req: Request, res: Response) => {
-    // Validate request ==> validate HTTP Protocol
+            const addVacs = new AddVacs(this.storage)
+            const vacs: VaccineModel[] = []
+            for (let i = 0; i < req.body.vaccines.length; i++) {
+                const vacJSON: JSON = req.body.vaccines[i];
+                const vaccine = VaccineModel.fromJSON(vacJSON)
+                vacs.push(vaccine)
+            }
+            try {
+                // storage.insertItems(items)
+                addVacs.call(vacs)
+            }
+            catch (pass) {
+                res.status(400).send()
+            }
+            res.status(201).send()
+        })
 
-    const addVacs = new AddVacs(storage)
-    const vacs: VaccineModel[] = []
-    for (let i = 0; i < req.body.vaccines.length; i++) {
-        const vacJSON: JSON = req.body.vaccines[i];
-        const vaccine = VaccineModel.fromJSON(vacJSON)
-        vacs.push(vaccine)
+        this.router.get(endpoint.ALL_VACCINES, (req: Request, res: Response) => {
+            res.send(this.storage.getAllVacs())
+        })
+
+        this.router.get(endpoint.VACCINE, (req: Request, res: Response) => {
+            let vac: VaccineModel | {} = {}
+            try {
+                vac = this.storage.countVac(req.body.vaccine.name)
+            }
+            catch (pass) {
+                res.status(400).send(vac)
+            }
+            res.send(vac)
+        })
     }
-    try {
-        // storage.insertItems(items)
-        addVacs.call(vacs)
-    }
-    catch (pass) {
-        res.status(400).send()
-    }
-    res.status(201).send()
-})
-
-router.get(endpoint.ALL_VACCINES, (req: Request, res: Response) => {
-    res.send(storage.getAllVacs())
-})
-
-router.get(endpoint.VACCINE, (req: Request, res: Response) => {
-    let vac: VaccineModel | {} = {}
-    try {
-        vac = storage.countVac(req.body.vaccine.name)
-    }
-    catch (pass) {
-        res.status(400).send(vac)
-    }
-    res.send(vac)
-})
-
-export = router
+}

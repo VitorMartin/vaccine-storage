5069dd8c6e2aaf7366703d631efaffd859656fcb
VitorMartin <vitor.martin.simoni@gmail.com>
2021-07-24 19:06:08 -0300

    Fixing extra "/" in endpoints

--- src/models/enums/fe_endpoints_enum.ts
+++ src/models/enums/fe_endpoints_enum.ts
@@ -3,7 +3,7 @@ import { routerEndpoints } from './router_endpoints_enum'
 export class feEndpoints { // FRONTEND ENDPOINTS. Class, but acts like enum: cannot parse variables as enums
     static SERVER_ROOT: string = routerEndpoints.ROOT
     static API_BASE_PATH: string = routerEndpoints.API_BASE_PATH
-    static PING : string = `${routerEndpoints.API_BASE_PATH}/${routerEndpoints.PING}`
-    static VACCINE : string = `${routerEndpoints.API_BASE_PATH}/${routerEndpoints.VACCINE}`
-    static ALL_VACCINES : string = `${routerEndpoints.API_BASE_PATH}/${routerEndpoints.ALL_VACCINES}`
+    static PING : string = `${routerEndpoints.API_BASE_PATH}${routerEndpoints.PING}`
+    static VACCINE : string = `${routerEndpoints.API_BASE_PATH}${routerEndpoints.VACCINE}`
+    static ALL_VACCINES : string = `${routerEndpoints.API_BASE_PATH}${routerEndpoints.ALL_VACCINES}`
 }
